57330ebecc5a44eb846c04032be367304d360691
jh-tccrocks <jhatfield@tccrocks.com>
2021-02-21 13:15:49 -0500

    Initial commit

---
+++ src/test/java/com/widgets_are_us/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widgets_are_us.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
